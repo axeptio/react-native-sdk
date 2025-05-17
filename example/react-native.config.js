1653ff6b747ea05c17d3b71acc16faa663dee691
PLB <pleberre@icloud.com>
2025-05-17 10:34:46 +0100

    chore: gradle to reference private repo

--- example/react-native.config.js
+++ example/react-native.config.js
@@ -1,6 +1,11 @@
 const path = require('path');
 
 module.exports = {
+  project: {
+    android: {
+      packageName: 'com.axeptiosdkexample',
+    },
+  },
   dependencies: {
     '@axeptio/react-native-sdk': {
       root: path.resolve(__dirname, '..'),
