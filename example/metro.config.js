b17d22177e1361738bc973b495e137209e34c162
PLB <pleberre@icloud.com>
2025-05-19 18:04:03 +0200

    chore: removing exclusion list from metro.config.js

--- example/metro.config.js
+++ example/metro.config.js
@@ -2,25 +2,9 @@ const path = require('path');
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 const root = path.resolve(__dirname, '..');
-const pak = require('../package.json');
-
-const modules = Object.keys(pak.peerDependencies || {});
 
 const config = {
   watchFolders: [root],
-
-  resolver: {
-    blockList: require('metro-config/src/defaults/exclusionList')(
-      modules.map(
-        (m) => new RegExp(`^${path.join(root, 'node_modules', m)}\\/.*$`)
-      )
-    ),
-
-    extraNodeModules: modules.reduce((acc, name) => {
-      acc[name] = path.join(__dirname, 'node_modules', name);
-      return acc;
-    }, {}),
-  },
 
   transformer: {
     getTransformOptions: async () => ({
